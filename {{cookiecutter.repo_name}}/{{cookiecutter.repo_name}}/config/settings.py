ce91bb5d69a01b286e900d36aab7c3dc2bde6fa0
LeadFocusWeb <billy@leadfocusmarketing.com>
2015-08-04 11:05:36 -0400

    nginx playbook changes, updated issues with user app

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
@@ -53,7 +53,7 @@ class Common(Configuration):
 
     # Apps specific for this project go here.
     LOCAL_APPS = (
-        'users',  # custom users app
+        'apps.users',  # custom users app
         # Your stuff: custom apps go here
     )
 
